df56a45e850cfb8dd546eea0096cff5d3ea3c51a
Apps247 <aprameyaithal@gmail.com>
2022-06-07 20:42:51 +0400

    Started dynamicizing CustomProceedButton

--- lib/shared/components/customProceedButton.dart
+++ lib/shared/components/customProceedButton.dart
@@ -1,32 +1,47 @@
 import 'package:flutter/material.dart';
 
+enum CustomProceedButtonType {
+  primary, // default
+  secondary, // Todo (apram): Implement
+}
+
 class CustomProceedButton extends StatelessWidget {
   const CustomProceedButton({
     Key? key,
     required this.title,
     this.heightFactor = 1,
+    this.customTextStyle,
+    this.onPressed,
+    this.customButtonType = CustomProceedButtonType.primary,
   }) : super(key: key);
 
   final String title;
   final double heightFactor;
+  final TextStyle? customTextStyle;
+  final CustomProceedButtonType customButtonType;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 73 * heightFactor,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: const Color(0xFF1F6FEB),
-      ),
-      alignment: Alignment.center,
-      child: Text(
-        title,
-        style: TextStyle(
-          color: Colors.white.withOpacity(.90),
-          fontFamily: 'Main',
-          fontSize: 19,
-          fontWeight: FontWeight.w800,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: 73 * heightFactor,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: const Color(0xFF1F6FEB),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          title,
+          style: customTextStyle ??
+              TextStyle(
+                color: Colors.white.withOpacity(.90),
+                fontFamily: 'Main',
+                fontSize: 19,
+                fontWeight: FontWeight.w800,
+              ),
         ),
       ),
     );
