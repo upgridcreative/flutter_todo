49aaba50d9226e4f23858fa0d69850db8553e0eb
Apps247 <aprameyaithal@gmail.com>
2022-08-01 13:37:41 +0400

    Added Mediaquery to fix size on widescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/authentication/welcome/welcome.dart';
+import 'package:flutter_todo/shared/components/habitTile.dart';
 import 'package:flutter_todo/theme/light.dart';
 import 'theme/extentions.dart';
 
@@ -12,10 +13,17 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return MaterialApp(
       theme: lightTextTheme,
-      home: const SafeArea(
-        child: WelcomeScreen(),
+      home: SafeArea(
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+                maxWidth: screenSize.width > 600 ? 600 : screenSize.width),
+            child: WelcomeScreen(),
+          ),
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
