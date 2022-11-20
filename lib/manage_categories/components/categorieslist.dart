30aee59802bc0e8e7ea7b852a8b49fe905dddefe
risheetajain <risheetajain309@gmail.com>
2022-11-20 13:59:12 +0530

    categorylist update

--- lib/manage_categories/components/categorieslist.dart
+++ lib/manage_categories/components/categorieslist.dart
@@ -1,8 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_todo/constants/decoration.dart';
 import 'package:flutter_todo/manage_categories/components/categoriestile.dart';
 import 'package:flutter_todo/models/category.dart';
@@ -14,11 +10,11 @@ class CategoryList extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal:18.0),
+      padding: const EdgeInsets.symmetric(horizontal: 18.0),
       child: Column(
         children: [
           SizedBox(
-            height: size.height*0.1*5,
+            height: size.height * 0.1 * 5,
             child: ReorderableList(
               itemCount: 5,
               itemBuilder: (BuildContext context, int index) {
@@ -31,16 +27,19 @@ class CategoryList extends StatelessWidget {
               onReorder: (int oldIndex, int newIndex) {},
             ),
           ),
-          Row(mainAxisAlignment: MainAxisAlignment.start,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.only(left:30),
+                padding: const EdgeInsets.only(left: 30),
                 child: Text("Create New", style: subTitle),
               ),
             ],
           ),
-          SizedBox(height: size.height*0.1,),
-          Text("Long Press To Drag",style: subTitle)
+          SizedBox(
+            height: size.height * 0.1,
+          ),
+          Text("Long Press To Drag", style: subTitle)
         ],
       ),
     );
