2e7b11fc0e5ad74e0377057342bab3995a513dd6
farhansyedain <farhansyedain@gmail.com>
2022-11-27 16:25:44 +0530

    Added back icon and padding

--- lib/shared/components/appbar.dart
+++ lib/shared/components/appbar.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_todo/constants/decoration.dart';
 
 class MyAppBar extends StatelessWidget {
   const MyAppBar({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [Text(title,style:largeTitle ,)],
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 25, right: 15),
+      child: Row(
+        children: [
+          const Icon(Icons.arrow_back_ios),
+          const SizedBox(width: 10),
+          Text(
+            title,
+            style: const TextStyle(
+              fontFamily: 'Main',
+              fontWeight: FontWeight.w600,
+              fontSize: 22,
+              color: Colors.black,
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
     );
   }
 }
