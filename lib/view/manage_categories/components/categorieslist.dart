484b4531cd53bea6efb5e6ad243ab92eb4388760
risheetajain <risheetajain309@gmail.com>
2022-12-06 22:57:00 +0530

    categorylist

--- lib/view/manage_categories/components/categorieslist.dart
+++ lib/view/manage_categories/components/categorieslist.dart
@@ -4,7 +4,6 @@ import 'package:flutter_todo/shared/theme/colors.dart';
 import 'package:flutter_todo/shared/theme/light.dart';
 
 import '../../../model/category.dart';
-import '../../../shared/components/customProceedButton.dart';
 import '../../../shared/components/custom_short_button.dart';
 import 'categoriestile.dart';
 
@@ -14,84 +13,86 @@ class CategoryList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-  int category=10;
+    int category = 10;
     return ListView.builder(
-          shrinkWrap: true,
-          itemCount: category+1,
-          itemBuilder: (BuildContext context, int index) {
-         if(index==category){
-             return  Column(
-      children: [
-      
-        Padding(
-          padding: const EdgeInsets.only(left: 15,top: 10),
-          child: InkWell(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    backgroundColor: ColorClass.grey,
-                    actionsPadding: EdgeInsets.only(right: 12),
-                    title: Text("Create new Category"),
-                    content: TextFormField(
-                        decoration: textFieldDecoration.copyWith(
-                            hintText: "e.g. Personal,work")),
-                    actions: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text("Cancel",
-                                  style: subTitle.copyWith(
-                                    color: ColorClass.black70,
-                                  ))),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          CustomShortButton(
-                            title: 'Save',
-                            heightFactor: 0.25,
-                            customTextStyle:
-                                subTitle.copyWith(color: Colors.white),
-                          )
-                        ],
-                      )
-                    ],
-                  );
-                },
-              );
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+        shrinkWrap: true,
+        itemCount: category + 1,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == category) {
+            return Column(
               children: [
-                Icon(Icons.add,color: mainColor,),
                 Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Text("Create New", style: subTitle),
+                  padding: const EdgeInsets.only(left: 15, top: 10),
+                  child: InkWell(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            backgroundColor: ColorClass.grey,
+                            actionsPadding: const EdgeInsets.only(right: 12),
+                            title: const Text("Create new Category"),
+                            content: TextFormField(
+                                decoration: textFieldDecoration.copyWith(
+                                    hintText: "e.g. Personal,work")),
+                            actions: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text("Cancel",
+                                          style: subTitle.copyWith(
+                                            color: ColorClass.black70,
+                                          ))),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  CustomShortButton(
+                                    title: 'Save',
+                                    heightFactor: 0.25,
+                                    customTextStyle:
+                                        subTitle.copyWith(color: Colors.white),
+                                  )
+                                ],
+                              )
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Icon(
+                          Icons.add,
+                          color: mainColor,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text("Create New", style: subTitle),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
+                SizedBox(
+                  height: size.height * 0.05,
+                ),
+                Text("Long Press To Drag",
+                    style:
+                        subTitle.copyWith(color: Colors.black.withOpacity(0.4)))
               ],
-            ),
-          ),
-        ),
-        SizedBox(
-          height: size.height * 0.05,
-        ),
-        Text("Long Press To Drag", style: subTitle.copyWith(color:Colors.black.withOpacity(0.4)))
-      ],
-    );
-          }else{
-return CategoriesTile(
+            );
+          } else {
+            return CategoriesTile(
               key: Key(index.toString()),
               category: Category(id: index),
               color: lightGreen,
             );
           }
-         }
-          
-        );
+        });
   }
 }
