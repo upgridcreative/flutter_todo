c7ed04a5d7ae76a3b171e9955ba56c2066766dcf
farhansyedain <farhansyedain@gmail.com>
2023-08-18 14:52:32 +0530

    wrap Inside safeArea

--- lib/view/authentication/reset_password/reset_password.dart
+++ lib/view/authentication/reset_password/reset_password.dart
@@ -14,81 +14,85 @@ class ResetPassword extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(ResetPasswordViewModel());
 
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 50),
-              const Text(
-                'Reset Password',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Metro',
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 1.2,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Obx(
-                () => Form(
-                  key: controller.resetPasswordFormKey,
-                  child: Column(
-                    children: [
-                      CustomTextField(
-                        hint: 'New Password',
-                        textFieldType: CustomTextFieldType.password,
-                        controller: controller.newPasswordTextController,
-                        validator: passwordValidator,
-                        enabled: !controller.disablePage.value,
-                        node: controller.newPasswordFocusNode,
-                        nextNode: controller.confirmNewPasswordFocusNode,
-                        autoFocus: true,
-                      ),
-                      const SizedBox(height: 5),
-                      CustomTextField(
-                        hint: 'Confirm Password',
-                        textFieldType: CustomTextFieldType.password,
-                        controller: controller.confirmNewPasswordTextController,
-                        enabled: !controller.disablePage.value,
-                        node: controller.confirmNewPasswordFocusNode,
-                        onFieldSubmitted: (value) => controller.resetPassword(),
-                           errorMessage: controller.misMatchingPasswords.value
-                          ? 'Passwords Don\'t Match'
-                          : null,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Obx(
-                () => controller.disablePage.value
-                    ? const CustomProgressButton(
-                        title: 'Resetting Password',
-                      )
-                    :  CustomProceedButton(
-                        title: 'Reset Password',
-                        heightFactor: 1,
-                        onPressed: controller.resetPassword,
-                      ),
-              ),
-              const SizedBox(height: 10),
-              const Center(
-                child: Text(
-                  'Forgot Password?',
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 50),
+                const Text(
+                  'Reset Password',
                   style: TextStyle(
-                    color: Colors.blue,
+                    color: Colors.black,
                     fontFamily: 'Metro',
-                    fontSize: 15,
+                    fontSize: 30,
                     fontWeight: FontWeight.w700,
+                    letterSpacing: 1.2,
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 20),
+                Obx(
+                  () => Form(
+                    key: controller.resetPasswordFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          hint: 'New Password',
+                          textFieldType: CustomTextFieldType.password,
+                          controller: controller.newPasswordTextController,
+                          validator: passwordValidator,
+                          enabled: !controller.disablePage.value,
+                          node: controller.newPasswordFocusNode,
+                          nextNode: controller.confirmNewPasswordFocusNode,
+                          autoFocus: true,
+                        ),
+                        const SizedBox(height: 5),
+                        CustomTextField(
+                          hint: 'Confirm Password',
+                          textFieldType: CustomTextFieldType.password,
+                          controller:
+                              controller.confirmNewPasswordTextController,
+                          enabled: !controller.disablePage.value,
+                          node: controller.confirmNewPasswordFocusNode,
+                          onFieldSubmitted: (value) =>
+                              controller.resetPassword(),
+                          errorMessage: controller.misMatchingPasswords.value
+                              ? 'Passwords Don\'t Match'
+                              : null,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Obx(
+                  () => controller.disablePage.value
+                      ? const CustomProgressButton(
+                          title: 'Resetting Password',
+                        )
+                      : CustomProceedButton(
+                          title: 'Reset Password',
+                          heightFactor: 1,
+                          onPressed: controller.resetPassword,
+                        ),
+                ),
+                const SizedBox(height: 10),
+                const Center(
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontFamily: 'Metro',
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
