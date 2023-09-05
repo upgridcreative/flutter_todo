723efded31ecfe2ab3c9746263457b54d69360c4
farhansyedain <farhansyedain@gmail.com>
2023-09-05 21:04:05 +0530

    save theme in shared prefrences

--- lib/view_model/theme_view_model.dart
+++ lib/view_model/theme_view_model.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/shared/prefs/sharedPrefrences.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeViewModel extends GetxController {
   late RxBool _isDarkTheme;
 
   ThemeViewModel() {
-    _isDarkTheme = Get.isDarkMode.obs;
+    _isDarkTheme =
+        (SharedPreferencesClass().prefs.getBool('DARK_MODE') ?? false).obs;
   }
 
   void toggleTheme() {
     _isDarkTheme.value = !_isDarkTheme.value;
+    SharedPreferencesClass().prefs.setBool('DARK_MODE', _isDarkTheme.value);
     Get.changeThemeMode(
       _isDarkTheme.value ? ThemeMode.dark : ThemeMode.light,
     );
